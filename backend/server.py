f039404cdbcc8529730f46e66af61d1ea9b72f27
Nico-Posada <posadanico@outlook.com>
2024-04-20 15:37:38 -0400

    pushed example of using camera and sending image every frame

--- backend/server.py
+++ backend/server.py
@@ -9,6 +9,22 @@ def serve_index():
     """ Serves the index.html file on the default server route """
     return app.send_static_file("index.html")
 
+@app.route("/camera")
+def serve_camera():
+    return app.send_static_file("backend/testing/camera.html")
+
+@app.route('/upload-image', methods=['POST'])
+def upload_image():
+    image = request.files['image']
+
+    # BytesIO
+    # data = image.stream
+
+    # Raw bytes
+    data = image.stream.read()
+    print(dir(image))
+    return jsonify({'message': 'Image uploaded successfully'})
+
 if __name__ == "__main__":
     # Replace with your desired host and port
     app.run(host="0.0.0.0", port=5000)
